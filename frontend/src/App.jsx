70259f80a858eb3374a035f4b25cf5c5ce02b857
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2023-02-23 15:01:05 +0100

    Revert "S02 carousel"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,12 @@
-import CategoriesList from "./components/categories/CategoriesList";
-import Navbar from "./components/navbar/Navbar";
-import Header from "./components/header/Header";
-import Cardfilmlist from "./components/cardfilmlist/Cardfilmlist";
+import Home from "./pages/Home";
+
 import "./App.css";
-import Footer from "./components/footer/Footer";
-// import Filminfo from "./components/filminfo/Filminfo";
-import Carousel from "./components/carousel/Carousel";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Navbar />
-      <Carousel />
-      <CategoriesList />
-      {/* <Filminfo /> */}
-      <Cardfilmlist />
-      <Footer />
+      <Home />
+      <p>coucou</p>
     </div>
   );
 }
